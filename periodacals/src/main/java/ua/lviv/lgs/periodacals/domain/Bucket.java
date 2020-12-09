4c83b33677dc218e3df98ae94c7b5493df2a2392
kostikroverist <kostikzhora@gmail.com>
2020-12-09 18:33:03 +0200

    add Bucket domain module

--- periodacals/src/main/java/ua/lviv/lgs/periodacals/domain/Bucket.java
+++ periodacals/src/main/java/ua/lviv/lgs/periodacals/domain/Bucket.java
@@ -1,4 +1,86 @@
 package ua.lviv.lgs.periodacals.domain;
 
+import java.sql.Date;
+import java.util.Objects;
+
 public class Bucket {
+
+    private Integer id;
+    private Integer userId;
+    private Integer productId;
+    private Date purchaseDate;
+
+    public Bucket() {
+    }
+
+    public Bucket(Integer id, Integer userId, Integer productId, Date purchaseDate) {
+        this.id = id;
+        this.userId = userId;
+        this.productId = productId;
+        this.purchaseDate = purchaseDate;
+    }
+
+    public Bucket(Integer userId, Integer productId, Date purchaseDate) {
+        this.userId = userId;
+        this.productId = productId;
+        this.purchaseDate = purchaseDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Bucket{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", productId=" + productId +
+                ", purchaseDate=" + purchaseDate +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public Date getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(Date purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Bucket)) return false;
+        Bucket bucket = (Bucket) o;
+        return Objects.equals(id, bucket.id) &&
+                Objects.equals(userId, bucket.userId) &&
+                Objects.equals(productId, bucket.productId) &&
+                Objects.equals(purchaseDate, bucket.purchaseDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userId, productId, purchaseDate);
+    }
 }
